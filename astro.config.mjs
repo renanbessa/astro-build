aafc692fc5f5e9105cb77f805ca6966ab1409aa4
Renan Bessa <renan.bessa@idinheiro.com.br>
2022-07-27 09:44:19 -0300

    feat: add vercel deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
-import { defineConfig } from 'astro/config';
-import critters from 'astro-critters';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
-
-import image from "@astrojs/image";
+import {defineConfig} from 'astro/config'
+import critters from 'astro-critters'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
+import image from '@astrojs/image'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), critters(), image()],
   experimental: {
-    integrations: true
-  }
-});
+    integrations: true,
+  },
+  output: 'server',
+  adapter: vercel(),
+})
